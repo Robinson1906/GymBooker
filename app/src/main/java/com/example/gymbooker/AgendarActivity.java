c29b153160596a4fa7e367cc1dadf15b48daf61e
Robinson <robinsonleonmoreno213@gmail.com>
2023-05-15 21:57:41 -0500

    Agregar conexion con find view y arreglar agendar

--- app/src/main/java/com/example/gymbooker/AgendarActivity.java
+++ app/src/main/java/com/example/gymbooker/AgendarActivity.java
@@ -1,13 +1,27 @@
 package com.example.gymbooker;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AgendarActivity extends AppCompatActivity {
 
+    private EditText txtRutina,TxtHora1,TxtHora2,TxtFecha;
+    private Button Agendar;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_agendar);
+
+        txtRutina = findViewById(R.id.txtRutina);
+        TxtHora1 = findViewById(R.id.TxtHora1);
+        TxtHora2 = findViewById(R.id.TxtHora2);
+        TxtFecha = findViewById(R.id.TxtFecha);
+
     }
 }
--- app/src/main/java/com/example/gymbooker/RegisterActivity.java
+++ app/src/main/java/com/example/gymbooker/RegisterActivity.java
@@ -1,14 +1,28 @@
 package com.example.gymbooker;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private TextView txtNombre,txtRegistro,txtCorreo,txtCedula,txtFnacimiento;
+    private Button btmContinuar;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        txtNombre = findViewById(R.id.txtnombre);
+        txtRegistro = findViewById(R.id.txtRegistro);
+        txtCorreo = findViewById(R.id.txtCorreo);
+        txtCedula = findViewById(R.id.txtCedula);
+        txtFnacimiento = findViewById(R.id.txtFnacimiento);
+
 
         Bundle b= getIntent().getExtras();
         Boolean b1=b.getBoolean("admin");
