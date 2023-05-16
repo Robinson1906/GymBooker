8881547d85cd6d19987d9f62322907713f0adbbc
Robinson <robinsonleonmoreno213@gmail.com>
2023-05-16 09:24:01 -0500

    HelperFecha

--- app/src/main/java/com/example/gymbooker/Helpers/HelperPersona.java
+++ app/src/main/java/com/example/gymbooker/Helpers/HelperPersona.java
@@ -1,13 +1,7 @@
 package com.example.gymbooker.Helpers;
 
-import android.view.View;
-import android.widget.Toast;
-
-import com.example.gymbooker.AgendarActivity;
-import com.example.gymbooker.Class.Reserva;
 import com.example.gymbooker.Class.User;
 import com.example.gymbooker.RetroFit.APIService;
-import com.example.gymbooker.RetroFit.ReservaService;
 import com.example.gymbooker.RetroFit.UserService;
 
 import java.util.ArrayList;
@@ -16,6 +10,8 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import java.util.Calendar;
+import java.util.Date;
 
 public class HelperPersona {
 
@@ -71,5 +67,46 @@ public class HelperPersona {
 
 
     public HelperPersona() {
+    }
+
+
+
+    public class HelperFechaUtil {
+
+        public static boolean verificarExpiracion(Date fecha) {
+            Calendar fechaActual = Calendar.getInstance();
+            Calendar fechaComparacion = Calendar.getInstance();
+            fechaComparacion.setTime(fecha);
+
+            // Comparar las fechas
+            if (fechaActual.after(fechaComparacion)) {
+                // La fecha ya ha pasado
+                return true;
+            }
+
+            // La fecha aún no ha pasado
+            return false;
+        }
+
+        public static void main(String[] args) {
+            // Obtener la fecha actual
+            Date fechaActual = new Date();
+
+            // Crear una fecha de ejemplo (2 días atrás)
+            Calendar cal = Calendar.getInstance();
+            cal.add(Calendar.DATE, -2);
+            Date fechaEjemplo = cal.getTime();
+
+            // Verificar si la fecha de ejemplo ha expirado
+            boolean expirada = verificarExpiracion(fechaEjemplo);
+
+            if (expirada) {
+                System.out.println("La fecha ha expirado");
+            } else {
+                System.out.println("La fecha aún no ha expirado");
+            }
+        }
+    }
+
     }
 }
