690cb64dc27075b057d7a071e0793d2fafbedddd
Robinson <robinsonleonmoreno213@gmail.com>
2023-05-16 09:26:28 -0500

    HelperFecha 2.0

--- app/src/main/java/com/example/gymbooker/Helpers/HelperPersona.java
+++ app/src/main/java/com/example/gymbooker/Helpers/HelperPersona.java
@@ -10,8 +10,6 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-import java.util.Calendar;
-import java.util.Date;
 
 public class HelperPersona {
 
@@ -69,44 +67,4 @@ public class HelperPersona {
     public HelperPersona() {
     }
 
-
-
-    public class HelperFechaUtil {
-
-        public static boolean verificarExpiracion(Date fecha) {
-            Calendar fechaActual = Calendar.getInstance();
-            Calendar fechaComparacion = Calendar.getInstance();
-            fechaComparacion.setTime(fecha);
-
-            // Comparar las fechas
-            if (fechaActual.after(fechaComparacion)) {
-                // La fecha ya ha pasado
-                return true;
-            }
-
-            // La fecha aún no ha pasado
-            return false;
-        }
-
-        public static void main(String[] args) {
-            // Obtener la fecha actual
-            Date fechaActual = new Date();
-
-            // Crear una fecha de ejemplo (2 días atrás)
-            Calendar cal = Calendar.getInstance();
-            cal.add(Calendar.DATE, -2);
-            Date fechaEjemplo = cal.getTime();
-
-            // Verificar si la fecha de ejemplo ha expirado
-            boolean expirada = verificarExpiracion(fechaEjemplo);
-
-            if (expirada) {
-                System.out.println("La fecha ha expirado");
-            } else {
-                System.out.println("La fecha aún no ha expirado");
-            }
-        }
-    }
-
-    }
 }
