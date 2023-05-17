7664c618f1e1edd6c03bfaec8c134a3ace42ad47
DFDev3 <ddiaz216@unab.edu.co>
2023-05-17 03:08:21 -0500

    FIRESTORE POST DONE

--- app/src/main/java/com/example/gymbooker/GoogleActivity.java
+++ app/src/main/java/com/example/gymbooker/GoogleActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GoogleActivity extends AppCompatActivity {
+    private String xd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
