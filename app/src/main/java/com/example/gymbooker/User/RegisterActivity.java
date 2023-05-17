ee7a86c12bc4b3a9e94252909a63aa23f467955b
DFDev3 <ddiaz216@unab.edu.co>
2023-05-16 23:37:44 -0500

    Nombre - Apellido

--- app/src/main/java/com/example/gymbooker/User/RegisterActivity.java
+++ app/src/main/java/com/example/gymbooker/User/RegisterActivity.java
@@ -16,7 +16,7 @@ import com.example.gymbooker.R;
 public class RegisterActivity extends AppCompatActivity {
     private SharedPreferences preferences;
 
-    private TextView txtnombre,txttelefono,txtcorreo,txtcedula,txtfnacimiento;
+    private TextView txtnombre,txttelefono,txtcorreo,txtcedula,txtfnacimiento,txtapellido;
     private Button btmcontinuar;
     @SuppressLint("MissingInflatedId")
     @Override
@@ -29,6 +29,7 @@ public class RegisterActivity extends AppCompatActivity {
         txtcorreo = findViewById(R.id.ed_correo);
         txtcedula = findViewById(R.id.ed_cedula);
         txtfnacimiento = findViewById(R.id.ed_nacimiento);
+        txtapellido = findViewById(R.id.ed_apellido);
         btmcontinuar = findViewById(R.id.btn_registrar);
     }
 
@@ -37,9 +38,10 @@ public class RegisterActivity extends AppCompatActivity {
         HelperPersona bInstance = new HelperPersona();
         User u = new User();
         u.setNombre(txtnombre.getText().toString());
-        u.setTelefono(txttelefono.getText().toString());
+        u.setApellido(txtapellido.getText().toString());
+        u.setTelefono(Long.parseLong(txttelefono.getText().toString()));
         u.setCorreo(txtcorreo.getText().toString());
-        u.setCedula(txtcedula.getText().toString());
+        u.setCedula(Long.parseLong(txtcedula.getText().toString()));
         u.setFechaNacimiento(txtfnacimiento.getText().toString());
         int response=bInstance.guardarPersona(u);
         if(response==200){
@@ -47,7 +49,7 @@ public class RegisterActivity extends AppCompatActivity {
             preferences=getSharedPreferences("gym-booker",MODE_PRIVATE);
             SharedPreferences.Editor editor= preferences.edit();
             editor.putInt("logged",1);
-            editor.putString("ccUsuario",u.getCedula());
+            editor.putString("ccUsuario",u.getCedula().toString());
             editor.apply();
             Intent i=new Intent(RegisterActivity.this, MainActivity.class);
             startActivity(i);
