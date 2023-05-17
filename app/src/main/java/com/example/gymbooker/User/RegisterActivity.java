40dbdeeb7af8a7d9400cd135ba36af6422efce1d
JuanYepes511 <jyepes511@unab.edu.co>
2023-05-17 00:21:22 -0500

    RegisterIntent1

--- app/src/main/java/com/example/gymbooker/User/RegisterActivity.java
+++ app/src/main/java/com/example/gymbooker/User/RegisterActivity.java
@@ -3,8 +3,10 @@ package com.example.gymbooker.User;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +14,10 @@ import android.widget.Toast;
 
 import com.example.gymbooker.MainActivity;
 import com.example.gymbooker.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
     private SharedPreferences preferences;
@@ -33,15 +39,33 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void onClickGuardar(View view){
-
         HelperPersona bInstance = new HelperPersona();
         User u = new User();
         u.setNombre(txtnombre.getText().toString());
         u.setTelefono(txttelefono.getText().toString());
-        u.setCorreo(txtcorreo.getText().toString());
         u.setCedula(txtcedula.getText().toString());
         u.setFechaNacimiento(txtfnacimiento.getText().toString());
         int response=bInstance.guardarPersona(u);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        u.setCorreo(txtcorreo.getText().toString()) ;
+        ActionCodeSettings actionCodeSettings = ActionCodeSettings.newBuilder()
+                .setAndroidPackageName("com.example.yourapp", true, null)
+                .setHandleCodeInApp(true)
+                .setUrl("https://gymbooker-c78de.firebaseapp.com/__/auth/action?mode=action&oobCode=code")
+                .build();
+        auth.sendSignInLinkToEmail(String.valueOf(txtcorreo), actionCodeSettings)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("Confirmacion", "Enlace de inicio de sesión enviado.");
+                            // Realiza las acciones adicionales después de enviar el enlace
+                        } else {
+                            Log.e("Confirmacion", "Error al enviar el enlace de inicio de sesión.", task.getException());
+                        }
+                    }
+                });
+
         if(response==200){
             Toast.makeText(this, "Registro Exitoso", Toast.LENGTH_SHORT).show();
             preferences=getSharedPreferences("gym-booker",MODE_PRIVATE);
